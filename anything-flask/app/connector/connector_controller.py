d8de3bfd079c486f900e89fe71cad6af87dbceaf
KrittapopManupornprapa <krittapop.manupornprapa@safecoms.asia>
2025-01-14 02:42:13 +0700

    feat: request connector

---
+++ anything-flask/app/connector/connector_controller.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, request
+from .helper.db_helper import DBHelper
+from .helper.api_helper import APIHelper
+
+main = Blueprint('main', __name__)
+
+# Route Testing
+@main.route('/api/test', methods=['GET'])
+def test_api():
+    return APIHelper.test_api()
+
+# POST /api/users - Create a new user
+@main.route('/api/users', methods=['POST'])
+def create_new_user():
+    body = request.get_json()
+    return DBHelper.create_user(body)

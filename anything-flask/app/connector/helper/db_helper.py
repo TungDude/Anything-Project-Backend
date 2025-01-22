64bcb4f8d0daac744d49e83536b4d27e55123e8d
KrittapopManupornprapa <krittapop.kms@gmail.com>
2025-01-22 21:50:57 +0700

    feat: create user password hash

--- anything-flask/app/connector/helper/db_helper.py
+++ anything-flask/app/connector/helper/db_helper.py
@@ -1,18 +1,18 @@
 from flask import jsonify
-from ...models import User
+from ...models.users.Users import User
+import bcrypt
 
 class DBHelper():
     def create_user(request_body):
         # Validate request_body
-        if 'username' not in request_body or 'email' not in request_body:
+        if 'username' not in request_body or 'password' not in request_body:
             return jsonify({
                 'error': 'Missing required fields'
             }), 400
 
         user = User(
             username=request_body['username'],
-            email=request_body['email'],
-            age=request_body.get('age', None)
+            password=bcrypt.hashpw(request_body['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
         )
         user.save()  # Save the user to MongoDB
 
